249253b68e76fc3ed287ffd5443a1f2d7e7e2b84
bence keresztes-nagy <telrodan@gmail.com>
2022-11-01 16:31:58 +0100

    fix css

--- src/app/features/side-menu/side-menu.component.ts
+++ src/app/features/side-menu/side-menu.component.ts
@@ -11,11 +11,11 @@ import { animate, style, transition, trigger } from '@angular/animations';
     trigger('fadeInAndOutAnimation', [
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('0.3s ease-in', style({ opacity: 1 })),
+        animate('0.3s 0.2s ease-in', style({ opacity: 1 })),
       ]),
       transition(':leave', [
         style({ opacity: 1 }),
-        animate('0.3s ease-in', style({ opacity: 0 })),
+        animate('0.1s ease-in', style({ opacity: 0 })),
       ]),
     ]),
   ],
